2bf00b159444e5e86757beb209fc74e08eee3070
0shai0 <148052932+0shai0@users.noreply.github.com>
2024-01-30 17:32:46 +0900

    feat: 메인페이지 구성
    pageable 라이브러리 사용, CSS 수정

---
+++ front/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Routes, Route} from "react-router-dom";
+import './css/Main.css';
+import Main from "./pages/Main";
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Main/>}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
