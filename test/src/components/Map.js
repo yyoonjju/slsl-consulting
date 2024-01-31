741bb4d7c65c7327ccab8a1685f9132685839c41
bigzero3949 <bigzero3949@gmail.com>
2024-01-31 19:41:31 +0900

    feat: 차트를 두 개로 나눈 후 지도와 날짜 기능이랑 연결

--- test/src/components/Map.js
+++ test/src/components/Map.js
@@ -23,27 +23,55 @@ function Map({data}) {
   ];
 
   return (
-    <ComposedChart
-      width={500}
-      height={400}
-      data={data === null ? blankData : data}
-      margin={{
-        top: 20,
-        right: 20,
-        bottom: 20,
-        left: 20
-      }}
-      className="chart"
-    >
-      <CartesianGrid stroke="#f5f5f5" />
-      <XAxis dataKey="time" />
-      <YAxis yAxisId="left" label={{value: "발전량(Mw)", offset: 10, angle: 0, position: "top", fontSize: "10px"}}/>
-      <YAxis yAxisId="right" label={{value: "누적발전량(Mw)", offset: 10, angle: 0, position: "top", fontSize: "10px"}} orientation="right"/>
-      <Tooltip />
-      <Legend />
-      <Bar yAxisId="right" dataKey="loc_total" barSize={20} fill="#413ea0" />
-      <Line yAxisId="left" type="monotone" dataKey="loc_power" stroke="#ff7300" />
-    </ComposedChart>
+    <div className="chart-container">
+      <div>
+        <ComposedChart
+          width={700}
+          height={400}
+          data={data === null ? blankData : data.slice(0,24)}
+          margin={{
+            top: 20,
+            right: 20,
+            bottom: 20,
+            left: 20
+          }}
+          className="chart"
+        >
+          <CartesianGrid stroke="#f5f5f5" />
+          <XAxis dataKey="time" />
+          <YAxis yAxisId="left" label={{value: "발전량(Mw)", offset: 10, angle: 0, position: "top", fontSize: "10px"}}/>
+          <YAxis yAxisId="right" label={{value: "누적발전량(Mw)", offset: 10, angle: 0, position: "top", fontSize: "10px"}} orientation="right"/>
+          <Tooltip />
+          <Legend />
+          <Bar yAxisId="right" dataKey="loc_total" barSize={20} fill="#413ea0" />
+          <Line yAxisId="left" type="monotone" dataKey="loc_power" stroke="#ff7300" />
+        </ComposedChart>
+      </div>
+
+      <div>
+          <ComposedChart
+          width={700}
+          height={400}
+          data={data === null ? blankData : data.slice(24,48)}
+          margin={{
+            top: 20,
+            right: 20,
+            bottom: 20,
+            left: 20
+          }}
+          className="chart"
+        >
+          <CartesianGrid stroke="#f5f5f5" />
+          <XAxis dataKey="time" />
+          <YAxis yAxisId="left" label={{value: "발전량(Mw)", offset: 10, angle: 0, position: "top", fontSize: "10px"}}/>
+          <YAxis yAxisId="right" label={{value: "누적발전량(Mw)", offset: 10, angle: 0, position: "top", fontSize: "10px"}} orientation="right"/>
+          <Tooltip />
+          <Legend />
+          <Bar yAxisId="right" dataKey="loc_total" barSize={20} fill="#413ea0" />
+          <Line yAxisId="left" type="monotone" dataKey="loc_power" stroke="#ff7300" />
+        </ComposedChart>
+      </div>
+    </div>
   );
 }
 
