4b69bc6c8c6f926c5495bd3e52160a8536b7aa7b
0shai0 <148052932+0shai0@users.noreply.github.com>
2024-01-31 20:06:35 +0900

    feat: 메인페이지 슬라이더 기능 추가

---
+++ front/src/pages/BtnSlider.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function BtnSlider({direction, moveSlide}) {
+
+
+    return(
+        <div>
+            <button onClick={moveSlide} className={direction === "prev" ?  
+            "btn-slide next" : "btn-slide prev"}>
+                <img src={direction === "prev" ? process.env.PUBLIC_URL + `/images/SlideNext.png` : process.env.PUBLIC_URL + `/images/SlidePrev.png`}/>
+            </button>
+            <button onClick={moveSlide} className={direction === "next" ?  
+            "btn-slide next" : "btn-slide prev" }>
+                <img src={direction === "next" ? process.env.PUBLIC_URL + `/images/SlideNext.png` : process.env.PUBLIC_URL + `/images/SlidePrev.png`}/>
+            </button>
+        </div>
+    )
+}
+
+export default BtnSlider;
--- front/src/pages/Main.js
+++ front/src/pages/Main.js
@@ -1,29 +1,42 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import pageable from "pageable";
-import { Link, useNavigate } from 'react-router-dom';
+import dataSlider from "./DataSlider";
+import BtnSlider from "./BtnSlider";
+import '../css/Slider.css';
+
+
 
 function Main() {
 
-    const navigate = useNavigate();
-
     useEffect(() => {
+
         const selector = ".container";
         let myPageable;
+
+
+        // pageable 라이브러리를 설정하기 위해 초기화 시키는 코드
     
         const initializePageable = () => {
-            myPageable = new pageable(selector, {
-                gap: 20,
-            });
+            myPageable = new pageable(selector);
         };
+
+
+        // scale이 커지거나, translateY로 이동을 시키기 위해
+        // 클래스 이름을 붙임. 위의 기능은 CSS로 구현
     
         const handleHashChange = () => {
             const hash = window.location.hash.substring(1);
             const targetSection = document.getElementById(hash);
+
+            // 스크롤하면 붙여졌던 클래스 이름을 없애는 코드
     
             document.querySelectorAll(".scaleChage, .fontUp1, .fontUp2, .fontUp3, .fontUp4, .fontUp5, .fontUp6, .fontShow1, .fontShow2, .fontShow3").forEach((element) => {
                 element.classList.remove("active");
                 element.classList.remove("load");
             });
+
+
+            // 스크롤 시 선언한 요소들에 active라는 클래스명을 붙이는 코드
     
             if (targetSection) {
                 const scaleChageElement = targetSection.querySelector(".scaleChage");
@@ -36,6 +49,7 @@ function Main() {
                 const fontShow1 = targetSection.querySelector(".fontShow1");
                 const fontShow2 = targetSection.querySelector(".fontShow2");
                 const fontShow3 = targetSection.querySelector(".fontShow3");
+
                 if (scaleChageElement) {
                     scaleChageElement.classList.add("active");
                 }
@@ -79,21 +93,33 @@ function Main() {
         };
     
         const destroyPageable = () => {
-            // Check if the container and myPageable exist before destroying pageable
             const container = document.querySelector(selector);
             if (container && myPageable) {
                 myPageable.destroy();
             }
         };
+
+
+        // 함수를 실행하는 코드
     
         handleHashChange();
         initializePageable();
     
         window.addEventListener("hashchange", handleHashChange);
+
+
+        // 원래는 scale이 0.8에서 1로 변경되는 애니매이션이 있지만
+        // 처음으로 페이지에 들어갈 때만 scale을 1로 만들어 애니매이션이
+        // 실행되지 않기 위해 다른 클래스 이름을 붙이고 CSS로 구현
     
         const firstScaleChageElement = document.querySelector(".scaleChage");
+        const fontUp1 = document.querySelector(".fontUp1");
+        const fontUp2 = document.querySelector(".fontUp2");
+
         if (firstScaleChageElement) {
             firstScaleChageElement.classList.add("load");
+            fontUp1.classList.add("active");
+            fontUp2.classList.add("active");
         }
     
         return () => {
@@ -102,6 +128,36 @@ function Main() {
         };
     }, []);
     
+
+
+    const [slideIndex, setSlideIndex] = useState(1);
+
+    // 버튼 클릭 시 뒤의 사진들로 이동하는 코드
+
+    const nextSlide = () => {
+        console.log("oh my god")
+
+        setSlideIndex(slideIndex === dataSlider.length - 1 ? 0 : slideIndex + 1);
+
+    }
+
+    // 버튼 클릭 시 전의 사진들로 이동하는 코드
+
+    const prevSlide = () => {
+
+        console.log("oh shit")
+
+        setSlideIndex(slideIndex === 0 ? dataSlider.length - 1 : slideIndex - 1);
+
+    }
+
+    useEffect(()=>{
+        const interval = setInterval(()=>{
+            nextSlide();        
+        }, 3000);
+        return () => clearInterval(interval);
+    });
+
 
     return (
         <div className="container" id="container">
@@ -119,33 +175,29 @@ function Main() {
             </section>
 
             <section data-anchor="Page 2" className="pg-page" id="page-2">
-                <div className="scaleChage">
-                    <div className="explain1">
-                        <p>
+                <div className="scaleChage"> 
+                {/* <p>
                         <span className="fontUp1">태양광 발전량</span>
-                        </p>
-                        <p>
+                    </p>
+                    <p>
                         <span className="fontUp2">태양광 수익률</span>
-                        </p>
-                        <a href="/introduce" className="fontShow1">제품 소개</a>
-                    </div>
-                    <div className="explain2">
-                        <p>
-                        <span className="fontUp3">태양광 발전량2</span>
-                        </p>
-                        <p>
-                        <span className="fontUp4">태양광 수익률2</span>
-                        </p>
-                        <a href="/introduce" className="fontShow2">제품 소개</a>
-                    </div>
-                    <div className="explain3">
-                        <p>
-                        <span className="fontUp5">태양광 발전량3</span>
-                        </p>
-                        <p>
-                        <span className="fontUp6">태양광 수익률3</span>
-                        </p>
-                        <a href="/introduce" className="fontShow3">제품 소개</a>
+                    </p>
+                    <a href="/introduce" className="fontShow1">제품 소개</a>  */}
+                    <div className="containerSlider">                              
+                        {dataSlider.map((obj, index) => {
+                            return (
+                                <div
+                                key={obj.id} 
+                                className= {slideIndex === index + 1 ?"slide active-anim" : "slide"}
+                                >
+                                    <img
+                                    src={process.env.PUBLIC_URL + `/images/Slider${slideIndex + 1}.jpg`}
+                                    />                                   
+                                </div>
+                            )
+                        })} 
+                        <BtnSlider moveSlide={nextSlide} direction={"next"}/> 
+                        <BtnSlider moveSlide={prevSlide} direction={"prev"}/>                                                      
                     </div>
                 </div>
             </section>
