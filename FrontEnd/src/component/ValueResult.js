a3be649fb7e25fcb3eff1bb5c4f6a02ac1b93003
notrowing <notrowing@naver.com>
2024-02-05 12:34:10 +0900

    ValueResult 결과 표시 설정

--- FrontEnd/src/component/ValueResult.js
+++ FrontEnd/src/component/ValueResult.js
@@ -6,6 +6,23 @@ const ValueResult = () => {
     const location = useLocation();
     const formData = location.state && location.state.formData;
 
+    console.log(formData);
+
+    //지역
+    const selectLocal = formData.selectLocal;
+
+    //패널
+    const selectPanel = formData.selectPanel;
+
+    //면적
+    const inputArea = formData.inputArea;
+
+    //시작익
+    const startDate = formData.startDate.toLocaleDateString('en-US', { year: 'numeric', month: '2-digit', day: '2-digit' });
+    
+    //종료일
+    const endDate = formData.endDate.toLocaleDateString('en-US', { year: 'numeric', month: '2-digit', day: '2-digit' });
+    
     if (!formData) {
         // formData가 없을 때 처리
         return <h2>No form data available.</h2>;
@@ -18,9 +35,12 @@ const ValueResult = () => {
                 <h1>발전수익 계산</h1>
             </div>
             <section className='ValueResult'>
-                <h2>ValueResult Page</h2>
-                <p>Received Form Data:</p>
-                <pre>{JSON.stringify(formData, null, 2)}</pre>
+                <div>{selectLocal}</div>
+                <div>{selectPanel}</div>
+                <div>{inputArea}</div>
+                <div>{startDate}</div>
+                <div>{endDate}</div>
+        
             </section>
 
         </article>
