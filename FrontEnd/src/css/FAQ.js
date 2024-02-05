4e32d5cb38e77467db79f044867b1f2589dc679d
0shai0 <148052932+0shai0@users.noreply.github.com>
2024-02-05 11:57:04 +0900

    주석 설명 추가

---
+++ FrontEnd/src/css/FAQ.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import FaqList from './FaqList';
+
+function FAQ() {
+
+    const [qnaUP , setQnAUP] = useState(false);
+    const [faqUP , setFaqUP] = useState(false);
+
+    useEffect(() => {
+        setQnAUP(true);
+        setFaqUP(true);
+    })
+
+    return(
+        <div className='QnA'>
+            <div className='QnAPhoto'>
+                <div className={`QnAPhotoBox ${qnaUP ? 'active' : ''}`}>
+                    <div className='QnATitle'>
+                        <h1>태양광 시작하려니<br/>막막하신가요?</h1>
+                        <p>SLSL에서 시작하면 쉬워져요!</p>
+                    </div>
+                    <div className='imgCover'>
+                        <div className='QnAImg'/>
+                    </div>
+                </div>
+            </div>
+            <div className="faq">
+                <div className={`faqText ${faqUP ? 'active' : ''}`}>
+                    {/* FaqItem, FaqList에서 받은 값을 아래 코드에 구현 */}
+                    <FaqList/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FAQ;
