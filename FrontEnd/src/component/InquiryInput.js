3e91d4b3d7dfca547365bd8a3bd3bef7f1237760
notrowing <notrowing@naver.com>
2024-02-06 16:33:57 +0900

    InquiryInput 수정

--- FrontEnd/src/component/InquiryInput.js
+++ FrontEnd/src/component/InquiryInput.js
@@ -249,13 +249,14 @@ const InquiryInput = () => {
 
                 </table>
 
-                <div >
+                <div>
                         개인정보 수집 및 이용 동의(필수)
                         <span className="red_span">&nbsp;*&nbsp;&nbsp;</span>
                         <input type='checkbox' id = "inputCheck"  onChange={handleChange} checked={InputData.inputCheck}/>
                 </div>
                 
                 <button id="inquiry_Btn" type='submit'>제출</button>
+
             </form>
 
         </section>
