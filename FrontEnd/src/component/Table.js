4c2bdf0f77d96f62840028fda212519919b4f087
bigzero3949 <bigzero3949@gmail.com>
2024-02-06 16:22:48 +0900

    feat: 발전량 예측 조회 페이지 레이아수 수정 및 기능 추가 시간 기능 map -> selectbox로 변경 Table 높이 제한 및 스크롤바 생성 Chart 중간선을 통해서 날짜 분리 및 title 추가

--- FrontEnd/src/component/Table.js
+++ FrontEnd/src/component/Table.js
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react';
 import Table from 'react-bootstrap/Table';
 import '../css/Table.css';
+import Moment from 'moment';
 
 function Tb({data}) {
     // Data를 가져오는 속도가 느릴 경우를 대비해서 빈 데이터 생성
@@ -18,53 +19,72 @@ function Tb({data}) {
 
     return (
         <Table striped bordered hover className="table">
-            <div className="test">
+            <div  className="tableContainer">
                 <div>
                     <thead>
                         <tr>
                             <th>날짜</th>
-                            <th colspan="2">firstDate</th>
+                            <th colspan="2">{data === null || data === undefined ? Moment(new Date()).format("YYYY-MM-DD") : data[0].date}</th>
+                            <th colspan="2">{data === null || data === undefined ? Moment(new Date()).add(1,"days").format("YYYY-MM-DD") : data[24].date}</th>
                         </tr>
                         <tr>
                             <th>시간</th>
                             <th>발전량(Mw)</th>
                             <th>누적발전량(Mw)</th>
+                            <th>발전량(Mw)</th>
+                            <th>누적발전량(Mw)</th>
                         </tr>
                     </thead>
-
-                    <tbody>
-                        {/* 첫 번째 날짜에 대한 데이터만 표시 */}
-                        {((data === null || data === undefined) ? blankData : data.slice(0,5)).map((row, index) => (
-                            <tr key={index}>
-                                <td className="td-test">{row.time}</td>
-                                <td>{row.loc_power}</td>
-                                <td>{row.loc_total}</td>
-                            </tr>
-                        ))}
-                    </tbody>
                 </div>
 
                 <div>
+                    <tbody className="scrollableBody">
+                        <div>
+                            {/* 첫 번째 날짜에 대한 데이터만 표시 */}
+                            {((data === null || data === undefined) ? blankData : data.slice(0,24)).map((row, index) => (
+                                <tr key={index}>
+                                    <td className="tdTime">{row.time}</td>
+                                    <td className="tdData1">{row.loc_power}</td>
+                                    <td className="tdData2">{row.loc_total}</td>
+                                </tr>
+                            ))}
+                        </div>
+                        
+                        <div className="tableData">
+                            {/* 두 번째 날짜에 대한 데이터만 표시 */}
+                            {((data === null || data === undefined) ? blankData : data.slice(24,48)).map((row, index) => (
+                                <tr key={index}>
+                                    <td className="tdData3">{row.loc_power}</td>
+                                    <td className="tdData4">{row.loc_total}</td>
+                                </tr>
+                            ))}
+                        </div>
+                    </tbody>
+                </div>
+
+                {/* <div>
                     <thead>
                         <tr>
-                            <th colspan="2">secondDate</th>
+                            <th colspan="2">{data === null || data === undefined ? Moment(new Date()).add(1,"days").format("YYYY-MM-DD") : data[24].date}</th>
                         </tr>
                         <tr>
                             <th>발전량(Mw)</th>
                             <th>누적발전량(Mw)</th>
                         </tr>
-                    </thead>
+                    </thead> */}
 
-                    <tbody>
-                        {/* 두 번째 날짜에 대한 데이터만 표시 */}
-                        {((data === null || data === undefined) ? blankData : data.slice(24,29)).map((row, index) => (
-                            <tr key={index}>
-                                <td>{row.loc_power}</td>
-                                <td>{row.loc_total}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </div>
+                    {/* <div className="tableData"> */}
+                        {/* <tbody> */}
+                            {/* 두 번째 날짜에 대한 데이터만 표시 */}
+                            {/* {((data === null || data === undefined) ? blankData : data.slice(24,48)).map((row, index) => (
+                                <tr key={index}>
+                                    <td className="tdData">{row.loc_power}</td>
+                                    <td className="tdData">{row.loc_total}</td>
+                                </tr>
+                            ))} */}
+                        {/* </tbody> */}
+                    {/* </div> */}
+                {/* </div> */}
             </div>
         </Table>
     );
