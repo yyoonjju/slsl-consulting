594a4e0f215c142b0628018f3755514d2fb1012c
bigzero3949 <bigzero3949@gmail.com>
2024-02-15 11:50:21 +0900

    style: 코드 주석 추가

--- BackEnd/src/main/java/com/example/slsl_server/controller/SearchController.java
+++ BackEnd/src/main/java/com/example/slsl_server/controller/SearchController.java
@@ -20,6 +20,7 @@ import com.example.slsl_server.repository.LandSmpPredictionDataRepository;
 import com.example.slsl_server.repository.PayRepository;
 
 @RestController
+// CORS 방지
 @CrossOrigin(origins = "*")
 public class SearchController {
     @Autowired
@@ -29,20 +30,27 @@ public class SearchController {
     @Autowired
     PayRepository payRepository;
 
+    // 지역, firstDate, secondDate를 받아서 데이터 가공 후 반환
     @GetMapping("/api/{location}")
     public List<Map<String,Object>> selectLocation(
         @PathVariable String location,
         @RequestParam("firstDate") String firstDate,
         @RequestParam("secondDate") String secondDate
     ) {
+        // 지역이 전국일 경우 지역을 통합해서 전국 평균으로 데이터 가공 후 반환
         if (location.equals("전국")) {
+            // 데이터 호출
             List<Map<String,Object>> data = dummyPowerDataRepository.findGroupByDateWithNativeQuery(LocalDate.parse(firstDate), LocalDate.parse(secondDate));
+            // 반환할 가공 데이터를 담을 박스 생성
             List<Map<String,Object>> result = new ArrayList<>();
+            // Math, Double, String 과정은 전국 평균으로 계산 시 type이 소수점 4자리의 double로 출력되기 때문에 정수값으로 반환하기 위해서 진행
             Long total = Math.round(Double.valueOf(String.valueOf(data.get(0).get("value"))));
+            // 박스에 가공 데이터를 담는 과정
             for (int i = 0; i < data.size(); i++) {
                 Map<String,Object> dataMap = new HashMap<>();
                 dataMap.put("date", data.get(i).get("date"));
                 dataMap.put("value", Math.round(Double.valueOf(String.valueOf(data.get(i).get("value")))));
+                // 누적발전량도 붙여서 반환하기 위한 데이터 가공 과정
                 if (i == 0) {
                     dataMap.put("total", total);
                 }
@@ -55,6 +63,8 @@ public class SearchController {
 
             return result;
         }
+        // 정확한 지역을 받을 경우 데이터 가공을 진행할 로직
+        // -> 전국 데이터 가공 로직과 거의 동일
         else {
             List<DummyPowerData> data = dummyPowerDataRepository.findByLocAndDateBetween(location, LocalDate.parse(firstDate), LocalDate.parse(secondDate));
             List<Map<String,Object>> result = new ArrayList<>();
@@ -79,6 +89,7 @@ public class SearchController {
         }
     };
 
+    // firstDate, secondDate를 받아서 데이터 가공 후 반환
     @GetMapping("/pay")
     public List<Pay> test(
         @RequestParam("firstDate") String firstDate,
