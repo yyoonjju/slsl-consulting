83dce6c5b04e7f56332ed9cfb4ce07275e264c15
bigzero3949 <bigzero3949@gmail.com>
2024-02-19 14:17:06 +0900

    fix: repository로 데이터를 불러오지 못하는 오류 수정 Date라는 column을 찾아오지 못하는 오류 발생 -> Repository에서 findByDateBetween을 findByDsBetween으로 수정

--- BackEnd/src/main/java/com/example/slsl_server/controller/ResultController.java
+++ BackEnd/src/main/java/com/example/slsl_server/controller/ResultController.java
@@ -4,12 +4,12 @@ import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.slsl_server.model.JejuSmpData;
 import com.example.slsl_server.repository.JejuSmpDataRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
@@ -22,7 +22,7 @@ public class ResultController {
         @RequestParam("firstDate") String firstDate,
         @RequestParam("secondDate") String secondDate
     ){
-        return jejuSmpDataRepository.findByDateBetween(LocalDate.parse(firstDate), LocalDate.parse(secondDate));
+        return jejuSmpDataRepository.findByDsBetween(LocalDate.parse(firstDate), LocalDate.parse(secondDate));
      
     }
 }
