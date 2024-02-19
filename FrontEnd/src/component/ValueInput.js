34ae08ffef9ade17f20cb6502b8adf79057eb83c
bigzero3949 <bigzero3949@gmail.com>
2024-02-19 21:17:45 +0900

    fix: 지역 선택 value 오류 수정

--- FrontEnd/src/component/ValueInput.js
+++ FrontEnd/src/component/ValueInput.js
@@ -208,23 +208,23 @@ ${MinAreaValue}보다 큰 숫자여야 합니다.`);
                                                 required
                                             >
                                             <option value=""disabled selected hidden>지역을 선택하세요.</option>
-                                            <option value="seoul" selected={selectedLocation === 'seoul'}>서울특별시</option>
-                                            <option value="daejeon" selected={selectedLocation === 'daejeon'}>대전광역시</option>
-                                            <option value="daegu" selected={selectedLocation === 'daegu'}>대구광역시</option>
-                                            <option value="busan" selected={selectedLocation === 'busan'}>부산광역시</option>
-                                            <option value="gwangju" selected={selectedLocation === 'gwangju'}>광주광역시</option>
-                                            <option value="incheon" selected={selectedLocation === 'incheon'}>인천광역시</option>
-                                            <option value="ulsan" selected={selectedLocation === 'ulsan'}>울산광역시</option>
-                                            <option value="sejong" selected={selectedLocation=== 'sejong'}>세종특별자치시</option>
-                                            <option value="gyeonggi" selected={selectedLocation === 'gyeonggi'}>경기도</option>
-                                            <option value="gangwon" selected={selectedLocation === 'gangwon'}>강원도</option>
-                                            <option value="north-chungcheong" selected={selectedLocation === 'north-chungcheong'}>충청북도</option>
-                                            <option value="south-chungcheong" selected={selectedLocation === 'south-chungcheong'}>충청남도</option>
-                                            <option value="north-gyeongsang" selected={selectedLocation === 'north-gyeongsang'}>경상북도</option>
-                                            <option value="south-gyeongsang" selected={selectedLocation=== 'south-gyeongsang'}>경상남도</option>
-                                            <option value="north-jeolla" selected={selectedLocation === 'north-jeolla'}>전라북도</option>
-                                            <option value="south-jeolla" selected={selectedLocation === 'south-jeolla'}>전라남도</option>
-                                            <option value="jeju" selected={selectedLocation === 'jeju'}>제주특별자치도</option>
+                                            <option value="서울" selected={selectedLocation === '서울'}>서울특별시</option>
+                                            <option value="대전" selected={selectedLocation === '대전'}>대전광역시</option>
+                                            <option value="대구" selected={selectedLocation === '대구'}>대구광역시</option>
+                                            <option value="부산" selected={selectedLocation === '부산'}>부산광역시</option>
+                                            <option value="광주" selected={selectedLocation === '광주'}>광주광역시</option>
+                                            <option value="인천" selected={selectedLocation === '인천'}>인천광역시</option>
+                                            <option value="울산" selected={selectedLocation === '울산'}>울산광역시</option>
+                                            <option value="세종" selected={selectedLocation=== '세종'}>세종특별자치시</option>
+                                            <option value="경기" selected={selectedLocation === '경기'}>경기도</option>
+                                            <option value="강원" selected={selectedLocation === '강원'}>강원도</option>
+                                            <option value="충북" selected={selectedLocation === '충북'}>충청북도</option>
+                                            <option value="충남" selected={selectedLocation === '충남'}>충청남도</option>
+                                            <option value="경북" selected={selectedLocation === '경북'}>경상북도</option>
+                                            <option value="경남" selected={selectedLocation=== '경남'}>경상남도</option>
+                                            <option value="전북" selected={selectedLocation === '전북'}>전라북도</option>
+                                            <option value="전남" selected={selectedLocation === '전남'}>전라남도</option>
+                                            <option value="제주" selected={selectedLocation === '제주'}>제주특별자치도</option>
                                         </select>
                                     </td>
                                 </tr>
